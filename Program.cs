be37e14779723c154dcf5dc7e1c8e93987f70a7b
Zeenat212 <24ntucsfl1219@student.ntu.edu.pk>
2025-03-25 12:08:42 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask6_Q1_
+{
+
+    public class product
+    {
+        public int ProductId;
+        public string Name;
+        public decimal Price;
+        public int QuantityinStock;
+
+        //Chaining Method 1
+        public void CheckQuantityinStock(int quantityinStock)
+        {
+            if (quantityinStock >= 0 && quantityinStock <= 100)
+            {
+                QuantityinStock = quantityinStock;
+            }
+            else
+            {
+
+                Console.WriteLine("Make sure Quantity in Stock should be in the range 0 - 100");
+                QuantityinStock = 0;
+            }
+
+
+        }
+
+
+        //public product(int productId, string Name, decimal price, int quantityinStock)
+        //{
+        //    ProductId = productId;
+        //    this.Name = Name;
+        //    Price = price;
+        //   CheckQuantityinStock(quantityinStock);
+
+        //}
+        // 3 Appraoch Contructor Chaining
+
+        //public product(int productId, string Name, decimal price) : this(productId, Name, price, 0)
+        //{
+        //    // No need to add anything
+        //}
+        //public product(int productId, int quantityinStock) : this(productId, "", 0, quantityinStock)
+        //{
+        //    // No need to add anything
+        //}
+        //public product() : this(0, "", 0, 0)
+        //{
+        //    // No need to add anything
+        //}
+        //public product(int productId, string Name) : this(productId, Name, 0, 0)
+        //{
+        //    // No need to add anything
+        //}
+        //**********************Done Method 03
+
+        //4 Approach Default values
+        public product(int productId = 0, string Name = "", decimal price = 0, int quantityinStock = 0)
+        {
+            ProductId = productId;
+            this.Name = Name;
+            Price = price;
+            CheckQuantityinStock(quantityinStock);
+
+        }
+        public void DisplayProductInfo()
+        {
+            Console.WriteLine($"Product ID = {ProductId},Name = {Name} Price = {Price}, Quantity = {QuantityinStock} ");
+        }
+        // Destructor
+        ~product()
+        {
+            Console.WriteLine($"Product Having ID {ProductId} is killed");
+        }
+        public class Store
+        {
+            public string StoreName;
+            public string Storelocation;
+            private product[] Products;
+            private int productCount;
+
+            //Constructor
+
+            public Store(string S_Name, string S_location)
+            {
+                StoreName = S_Name;
+                Storelocation = S_location;
+                Products = new product[10];
+                productCount = 0;
+            }
+
+            //Method
+
+            public void AddProductinStore(product Product)
+            {
+                if (productCount < Products.Length)
+                {
+                    Products[productCount++] = Product;
+                }
+                else
+                {
+                    Console.WriteLine("Not add products");
+                }
+            }
+            public void DisplayAllProducts()
+            {
+                Console.WriteLine($"All Inventory of the store {StoreName} at location {Storelocation} is here");
+                foreach (var Dummy_Product in Products)
+                {
+                    if(Dummy_Product!=null)
+                    Dummy_Product.DisplayProductInfo();
+                }
+            }
+            ~Store()
+            {
+                Console.WriteLine("stored Application Closed");
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                product p1= new product();  //Default
+                product p2= new product(1,"camera",25.5m,10);//All Values
+                product p3 = new product(2, "Mouse", 10.23m); //No Quantity in Stock 4 Apprach Default Values
+                product p4 = new product(3,"",0, 23);//No name and No price 3 Approach
+                product p5 = new product(4, "Keyboard"); //Only iD and name
+
+                Store store = new Store("Zeenat","FSD");
+                store.AddProductinStore(p1);    
+                store.AddProductinStore(p2);
+                store.AddProductinStore(p3);
+                store.AddProductinStore(p4);
+                store.AddProductinStore(p5);
+
+                store.DisplayAllProducts();
+                Console.ReadLine();
+
+                Console.WriteLine("Here we are killing objects");
+
+                //How to Handle Destructor
+                p1 = null;
+                p2 = null;
+                p3 = null;
+                p4 = null;
+                p5 = null;
+                store = null;
+
+                //Run Garbage Value
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HomeTask6(Q1)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeTask6(Q1)")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("72b1c5a8-e312-4537-a633-9d4814e5561a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
